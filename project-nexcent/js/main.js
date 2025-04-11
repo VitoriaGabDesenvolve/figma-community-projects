678a232964986df81194a460af22f4055f7792aa
Vitória Santos <161534498+VitoriaGabDesenvolve@users.noreply.github.com>
2025-04-10 22:55:20 -0300

    Ativação do slide

--- project-nexcent/js/main.js
+++ project-nexcent/js/main.js
@@ -4,3 +4,18 @@ function clickMenu() {
     let menu = document.querySelector('.header__menu_itens');
     menu.classList.toggle('ativo');
 }
+
+// Slide
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
